3a8db76fb520276f9c8d17d34ec5bc9e2a896a7f
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-16 00:01:55 +0400

    added layout component for testimonials section

---
+++ src/layout/SectionTestimonials.js
@@ -0,0 +1,51 @@
+import Testimonial from "../components/Testimonial";
+import src1 from "../img/profile-1.jpg";
+import src2 from "../img/profile-2.jpg";
+import src3 from "../img/profile-3.jpg";
+
+const SectionTestimonials = () => {
+  return (
+    <section className="section-testimonials">
+      <div className="container">
+        <Testimonial
+          text="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+          become a well-oiled collaboration machine."
+          cla
+          src={src1}
+          alt="User Image"
+          className="section-testimonials"
+          textName="Satish Patel"
+          userClassName="user"
+          textCredentials="Founder & CEO, Huddle"
+          credentialsClassName="credentials"
+        />
+        <Testimonial
+          text="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+          become a well-oiled collaboration machine."
+          cla
+          src={src2}
+          alt="User Image"
+          className="section-testimonials"
+          textName="Satish Patel"
+          userClassName="user"
+          textCredentials="Founder & CEO, Huddle"
+          credentialsClassName="credentials"
+        />
+        <Testimonial
+          text="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+          become a well-oiled collaboration machine."
+          cla
+          src={src3}
+          alt="User Image"
+          className="section-testimonials"
+          textName="Satish Patel"
+          userClassName="user"
+          textCredentials="Founder & CEO, Huddle"
+          credentialsClassName="credentials"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default SectionTestimonials;
