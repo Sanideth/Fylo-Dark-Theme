ad13bfeaab82492e03c359be935e81a8df13ed14
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-16 22:15:01 +0400

    added inner wrapper to the section

--- src/layout/SectionTestimonials.js
+++ src/layout/SectionTestimonials.js
@@ -7,42 +7,44 @@ const SectionTestimonials = () => {
   return (
     <section className="section-testimonials">
       <div className="container">
-        <Testimonial
-          text="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+        <div className="section-testimonials__testimonials">
+          <Testimonial
+            text="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
           become a well-oiled collaboration machine."
-          cla
-          src={src1}
-          alt="User Image"
-          className="section-testimonials"
-          textName="Satish Patel"
-          userClassName="user"
-          textCredentials="Founder & CEO, Huddle"
-          credentialsClassName="credentials"
-        />
-        <Testimonial
-          text="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+            cla
+            src={src1}
+            alt="User Image"
+            className="section-testimonials"
+            textName="Satish Patel"
+            userClassName="user"
+            textCredentials="Founder & CEO, Huddle"
+            credentialsClassName="credentials"
+          />
+          <Testimonial
+            text="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
           become a well-oiled collaboration machine."
-          cla
-          src={src2}
-          alt="User Image"
-          className="section-testimonials"
-          textName="Satish Patel"
-          userClassName="user"
-          textCredentials="Founder & CEO, Huddle"
-          credentialsClassName="credentials"
-        />
-        <Testimonial
-          text="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+            cla
+            src={src2}
+            alt="User Image"
+            className="section-testimonials"
+            textName="Satish Patel"
+            userClassName="user"
+            textCredentials="Founder & CEO, Huddle"
+            credentialsClassName="credentials"
+          />
+          <Testimonial
+            text="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
           become a well-oiled collaboration machine."
-          cla
-          src={src3}
-          alt="User Image"
-          className="section-testimonials"
-          textName="Satish Patel"
-          userClassName="user"
-          textCredentials="Founder & CEO, Huddle"
-          credentialsClassName="credentials"
-        />
+            cla
+            src={src3}
+            alt="User Image"
+            className="section-testimonials"
+            textName="Satish Patel"
+            userClassName="user"
+            textCredentials="Founder & CEO, Huddle"
+            credentialsClassName="credentials"
+          />
+        </div>
       </div>
     </section>
   );
