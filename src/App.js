9a198202b22763fb7fb967a9bdb1fbb09967b9d4
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-19 19:19:50 +0400

    changed css file to the prefixed one

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.prefixed.css";
 import Header from "./layout/Header";
 import SectionBonuses from "./layout/SectionBonuses";
 import SectionRemote from "./layout/SectionRemote";
